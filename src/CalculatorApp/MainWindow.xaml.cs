d863a6f3ad2fdeb2d99ad0c037062b425a5e570a
Sayed <project.khawadi@gmail.com>
2026-06-26 14:15:17 +0200

    Add basic button input logic

--- src/CalculatorApp/MainWindow.xaml.cs
+++ src/CalculatorApp/MainWindow.xaml.cs
@@ -1,24 +1,49 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CalculatorApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is not Button button)
+                return;
+
+            string value = button.Content.ToString() ?? "";
+
+            if (value == "." && DisplayText.Text.Contains("."))
+                return;
+
+            if (DisplayText.Text == "0" && value != ".")
+            {
+                DisplayText.Text = value;
+            }
+            else
+            {
+                DisplayText.Text += value;
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayText.Text = "0";
+        }
+
+        private void BackspaceButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (DisplayText.Text.Length <= 1)
+            {
+                DisplayText.Text = "0";
+                return;
+            }
+
+            DisplayText.Text = DisplayText.Text[..^1];
         }
     }
 }
